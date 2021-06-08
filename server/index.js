114997d40748ccfc87cfccb06bbdf3dbf38d8497
DarylvdBerg <darylvdberg@gmail.com>
2021-06-08 23:59:46 +0200

    Removed client id and secret

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const cors = require('cors');
 const app = express();
 app.use(cors());
 
-var clientId = "b381cea0329f438bb48748b68eab820a";
-var clientSecret = "9a75a7ba93f247bc8822c80642c48317";
-var playlistId = "4ElSuytngDHoYR5gM2Z1zx";
+var clientId = process.env.SPOTIFY_CLIENT_ID;
+var clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+var playlistId = process.env.SPOTIFY_PLAYLIST_ID;
 
 var spotifyApi = new SpotifyWebApi({
     clientId: clientId,
@@ -16,6 +17,7 @@ var spotifyApi = new SpotifyWebApi({
 spotifyApi.clientCredentialsGrant().then(function(data) {
     spotifyApi.setAccessToken(data.body['access_token'])
 }, function(error) {
+    console.log(error);
     console.log("error retrieving the code");
 });
 
